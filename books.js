c77daad55f9201423f03e044cc90da51cc970a28
Chandanashree <chandanashree@users.noreply.github.com>
2016-04-10 14:51:09 -0700

    JS file for books list

---
+++ books.js
@@ -0,0 +1,13 @@
+function boyFriendsBooks(){
+   var list = [
+   {name: "Colleen Hoover", book:"Hopeless"   },
+   {name: "John Green", book:"The Fault in Our Stars"}
+   ]
+}
+
+function boyFriendsBooks(){
+   var list = [
+   {name: "Cassandra Clare", book:"Clockwork Angel"   },
+   {name: " Cassandra Clare", book:"City of Glass"}
+   ]
+"
