9566ca22d864c118cee406400424a497ebf488af
chandanashree <chandanashree7@gmail.com>
2016-04-10 15:22:13 -0700

    Merged files using stash and merge cmd

--- books.js
+++ books.js
@@ -2,6 +2,8 @@ function boyFriendsBooks(){
    var list = [
    {name: " J.A. Redmerski", book:"The Edge of Never "},
    {name: "Colleen Hoover", book:"Hopeless"   },
+   {name: "Veronica Roth", book:"Divergent"   },
+   {name:"Colleen Hoover", book:"Slammed"}
    {name: "John Green", book:"The Fault in Our Stars"}
    ]
 }
